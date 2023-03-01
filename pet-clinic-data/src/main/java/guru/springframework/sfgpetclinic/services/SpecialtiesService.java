5d90ff1bedc9b1d2a3fdb867fcca02086d00e06b
Matthew Rada <matthewr7.christopher@gmail.com>
2023-03-01 07:06:41 -0500

    added specialties Map Impl. close #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+/**
+ * Created by FusRada on March 01, 2023
+ */
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
